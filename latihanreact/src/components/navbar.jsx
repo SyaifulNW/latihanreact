07476ac70f04b4f9d3533fa46b5a88e6cec9628d
Syaiful Nur Wardani <syaifulnurw123@gmail.com>
2024-09-15 22:24:40 +0700

    init

---
+++ latihanreact/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <div className="n">
+      <h1 className="text-center text-green-600 text-3xl font-bold">ikan</h1>
+
+
+      <div className="flex flex-col items-start gap-10">
+
+      <div className="px-4 py-2  rounded-lg ml-10  bg-yellow-300">login</div>
+      
+      
+      <button className="px-4 py-2 rounded-lg ml-10  bg-yellow-300">sign in</button>
+      </div>
+
+    </div>
+  );
+} 
+
+export default Navbar;
+
